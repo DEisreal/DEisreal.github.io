0b9beafccbefcd4709c4d9f49b0bb1cdc5bc4c0f
DEisreal <374932969@qq.com>
2019-01-03 13:32:35 +0800

    enter

--- 19.01.03信号大作业/程序_3170406037_李易潼_3170406034_毛佳雁_3170406036_祝立群.py
+++ 19.01.03信号大作业/程序_3170406037_李易潼_3170406034_毛佳雁_3170406036_祝立群.py
@@ -4,6 +4,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import animation
+plt.rcParams['animation.convert_path'] = 'D:\RouseWeiser\ImageMagick-6.2.7-Q16\convert.exe'
 
 fig, ax = plt.subplots() 
 #建立一个fig对象，建立一个axis1对象
@@ -11,7 +12,7 @@ ax = plt.gca()
 #获取ax图例Convolution signal process
 plt.rcParams['font.sans-serif']=['SimHei']
 plt.rcParams['axes.unicode_minus']=False
-ax.set(xlim=[-15, 15], ylim=[0, 6], title='卷积信号动画演示',
+ax.set(xlim=[-5, 5], ylim=[0, 4], title='卷积信号动画演示',
        ylabel='Y-轴', xlabel='X-轴')
 #设置x轴y轴范围及标题
 ax.spines['right'].set_color('none')
@@ -22,20 +23,20 @@ ax.spines['bottom'].set_position(('data', 0))
 ax.yaxis.set_ticks_position('left')
 ax.spines['left'].set_position(('data', 0))
 #获取指定的x区间的数据
-ω=np.pi/4
-x = np.linspace(-10, 0, 1000)
-x1 = np.linspace(-13, -8, 1000)
+
+x = np.linspace(-1, 1, 1000)
+x1 = np.linspace(-8, 1, 1000)
 x_data, y_data = [], []
-y = 4*np.sin(ω*x)        #控制固定函数
+y = x1/x1        #控制固定函数
 y[0] = 0
 y[-1] = 0
 ax.plot(x, y, "red")
 def init():
-    y1 = 2*np.sin(x1)
+    y1 = x1/x1
     line.set_data(x1,y1)
     return line,
 def animate(i):
-    x2 = np.linspace(-13 + i, -8 + i, 1000)
+    x2 = np.linspace(-1 + i, 1 + i, 1000)
     y2 = x1/x1            #控制卷积函数
     y2[0] = 0
     y2[-1] = 0
@@ -43,36 +44,28 @@ def animate(i):
     if i == 0:
         global x_data, y_data
         x_data, y_data = [], []
-        line1, = ax.plot(x_data, y_data, "yellow")
+        line1, = ax.plot(x_data, y_data)
 
-    if  i >= 6 and i < 8:
-        x_data.append(-8+i)
-        y_data.append(2*(-8+i)+2)
+    if  i>=-3 and i < 1:
+        x_data.append(i - 4)
+        y_data.append(2+(i - 4))
         line1, = ax.plot(x_data, y_data, "yellow")
-      
         return line, line1,
-    if i >= 8 and i < 11:
-        x_data.append(-8+i)
-        y_data.append(4)
-        line1, = ax.plot(x_data, y_data, "yellow")
 
-        return line, line1,
-    elif i >= 11:
-        x_data.append(-8+i)
-        y_data.append(2*(4-(-8+i)))
+    elif  i >= 1:
+        x_data.append(i - 1)
+        y_data.append(2-(i - 1))
         line1, = ax.plot(x_data, y_data, "yellow")
         return line, line1,
 
     return line,
 
-y3 = 2*np.sin(x1)
+y3 = x1/x1
 y3[0] = 2
-y3[-1] = 0
+y3[-2] = 0
 line, = ax.plot(x1, y3, "black")    #控制移动函数
 
-
-anim1=animation.FuncAnimation(fig, animate, init_func=init, frames=30, interval=100, blit=True) 
-#30帧的动画并且帧与帧之间间隔100ms，blit是一个非常重要的关键字，它告诉动画只重绘修改的部分，结合上面保存的时间， blit=true会使动画显示得会非常非常快
-
-
+# call the animator.  blit=true means only re-draw the parts that have changed.
+anim1=animation.FuncAnimation(fig, animate, init_func=init, frames=30, interval=1000, blit=True) 
+anim1.save('exxxo.gif', fps=2, writer='imagemagick')
 plt.show()
--- SandS/big work.py
+++ SandS/big work.py
@@ -54,3 +54,84 @@ plt.figure("卷积")
 plt.title("y(t)——Y(jw)；X(jw)*Y(jw)——x(t)·h(t)") 
 plt.plot(b)
 plt.show()
+
+
+
+# -*- coding: UTF-8 -*-
+#coding=utf-8
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import animation
+
+fig, ax = plt.subplots() 
+#建立一个fig对象，建立一个axis1对象
+ax = plt.gca() 
+#获取ax图例Convolution signal process
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus']=False
+ax.set(xlim=[-15, 15], ylim=[0, 6], title='卷积信号动画演示',
+       ylabel='Y-轴', xlabel='X-轴')
+#设置x轴y轴范围及标题
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+#隐藏轴上边和右边
+ax.xaxis.set_ticks_position('bottom')
+ax.spines['bottom'].set_position(('data', 0))
+ax.yaxis.set_ticks_position('left')
+ax.spines['left'].set_position(('data', 0))
+#获取指定的x区间的数据
+ω=np.pi/4
+x = np.linspace(-10, 0, 1000)
+x1 = np.linspace(-13, -8, 1000)
+x_data, y_data = [], []
+y = 4*np.sin(ω*x)        #控制固定函数
+y[0] = 0
+y[-1] = 0
+ax.plot(x, y, "red")
+def init():
+    y1 = 2*np.sin(x1)
+    line.set_data(x1,y1)
+    return line,
+def animate(i):
+    x2 = np.linspace(-13 + i, -8 + i, 1000)
+    y2 = x1/x1            #控制卷积函数
+    y2[0] = 0
+    y2[-1] = 0
+    line.set_data(x2,y2)
+    if i == 0:
+        global x_data, y_data
+        x_data, y_data = [], []
+        line1, = ax.plot(x_data, y_data, "yellow")
+
+    if  i >= 6 and i < 8:
+        x_data.append(-8+i)
+        y_data.append(2*(-8+i)+2)
+        line1, = ax.plot(x_data, y_data, "yellow")
+      
+        return line, line1,
+    if i >= 8 and i < 11:
+        x_data.append(-8+i)
+        y_data.append(4)
+        line1, = ax.plot(x_data, y_data, "yellow")
+
+        return line, line1,
+    elif i >= 11:
+        x_data.append(-8+i)
+        y_data.append(2*(4-(-8+i)))
+        line1, = ax.plot(x_data, y_data, "yellow")
+        return line, line1,
+
+    return line,
+
+y3 = 2*np.sin(x1)
+y3[0] = 2
+y3[-1] = 0
+line, = ax.plot(x1, y3, "black")    #控制移动函数
+
+
+anim1=animation.FuncAnimation(fig, animate, init_func=init, frames=30, interval=100, blit=True) 
+#30帧的动画并且帧与帧之间间隔100ms，blit是一个非常重要的关键字，它告诉动画只重绘修改的部分，结合上面保存的时间， blit=true会使动画显示得会非常非常快
+
+
+plt.show()
